55279e618d3b051a6b6cb681f183d39b11e658c6
antonio-treelogic <antonio-treelogic>
2022-07-01 13:31:43 +0000

    added lambda and fix script.sh

--- module/container/to_copy/cron_files/script.sh
+++ module/container/to_copy/cron_files/script.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 
 n_connections=`netstat -tnp | grep ESTABLISHED | wc -l`
-websocket_id= $websocketId #`echo "${internalID}${interWorkingInterface}${platformID}" | sha1sum| awk '{print $1}'`
 db_path=/home/ec2-user/vol/websockets.db
 
 if [ $n_connections -gt 2 ]
 then
         echo 'Websocket connection is up'
-        sqlite3 $db_path  'UPDATE websockets set is_active=1, count_err=0 where websocket_id ="'${websocket_id}'"'
+        sqlite3 $db_path  'UPDATE websockets set is_active=1, count_err=0 where websocket_id ="'${websocketId}'"'
 else
-        sqlite3 $db_path 'UPDATE websockets set count_err=count_err+1 where websocket_id ="'${websocket_id}'"'
-        retrys=`sqlite3 $db_path 'select count_err from websockets where websocket_id="'${websocket_id}'"'`
+        sqlite3 $db_path 'UPDATE websockets set count_err=count_err+1 where websocket_id ="'${websocketId}'"'
+        retrys=`sqlite3 $db_path 'select count_err from websockets where websocket_id="'${websocketId}'"'`
         echo 'Websocket connection id down, retry n:'$retrys
         if [ $retrys -gt 2 ]
         then
-                sqlite3 $db_path  'UPDATE websockets set is_active=0,count_err=0 where websocket_id ="'${websocket_id}'"'
+                sqlite3 $db_path  'UPDATE websockets set is_active=0,count_err=0 where websocket_id ="'${websocketId}'"'
                 kill $( pidof java )
         fi
 fi
---
+++ module/lambda/lambda_function.py
@@ -0,0 +1,155 @@
+
+import time
+import json
+import boto3
+import hashlib
+
+ssm = boto3.client("ssm")
+connection_params={}
+user_params = {}
+
+connection_params['instanceId'] ='i-079c167e9689d84da'
+#connection_params['instanceId'] ='i-0781c88c895268a65'
+
+#volume name on the given ec2-instance
+vol_name = 'websocket_state'
+path_to_db='/var/lib/docker/volumes/'+vol_name+'/_data/websockets.db'
+##path_to_db='/var/lib/docker/volumes/my-vol/_data/websockets.db'
+# Need to have image available on the EC2 instance
+image_name = 'websocket'
+
+
+
+
+def get_hash(connection_params):
+    connection_exclude={x: connection_params[x] for x in connection_params if x not in {"instanceId"}}
+    return hashlib.sha1(''.join(connection_exclude.values()).encode('utf-8')).hexdigest()
+
+def send_command(command, response=False):
+    response = ssm.send_command(
+                InstanceIds=[connection_params['instanceId']],
+                DocumentName="AWS-RunShellScript",
+                Parameters={
+                    "commands": [command]
+                },  # replace command_to_be_executed with command
+            )
+    if not response:
+        return 'Command executed'
+        # fetching command id for the output
+    else:
+        command_id = response["Command"]["CommandId"]
+        time.sleep(3)
+        return ssm.get_command_invocation(CommandId=command_id, InstanceId=connection_params['instanceId'])
+    
+    
+def get_state(container_id):
+    
+    """ Gets the state of the given connection. It can either be 0,1 or an empty string """
+
+    command = "sqlite3 "+path_to_db+" 'select is_active from websockets where websocket_id =\""+container_id+"\"'"
+    return send_command(command, response=True)['StandardOutputContent'].strip()
+
+
+def remove_socket(container_id):
+    
+    """ stop the container running that socket """
+
+    command_1 = 'docker stop '+container_id
+    send_command(command_1)
+    
+    #update the websockets table in sqlite3
+    command_2 = "sqlite3 "+path_to_db+" 'update websockets set is_active=0 where websocket_id=\""+container_id+"\"'"
+    send_command(command_2)
+    return 'Websocket closed'
+    
+ 
+def docker_run_command(websocket_id):
+    
+    """returns a string with the command that should be run to create the docker container
+        so the websocket is created for the given parameters.
+        Note that the image has to be already built """
+    
+    return  """docker run -itd -v {}:/home/ec2-user/vol --name {} \
+                -e user='{}' \
+                -e pass='{}' \
+                -e internalID='{}' \
+                -e interWorkingInterface='{}' \
+                -e platformID='{}' \
+                -e websocketId='{}' \
+                {} """.format(vol_name,
+                                websocket_id,
+                                user_params['asapa_email'],
+                                user_params['asapa_pass'] ,
+                                connection_params['internalID'],
+                                connection_params['interWorkingInterface'],
+                                connection_params['platformID'],
+                                websocket_id, 
+                                image_name) 
+
+def open_socket(state, websocket_id):
+
+    """ it creates a websocket by running a container with the given interpace, resource and platform parameter.
+        If the container exists and is running it will do nothing
+        if exists and is not running it will do docker start
+        if its a new connection it will create a new container by docker run """
+    if(state=='1'):
+        return {'body':'Do nothing', 'Status': 'Success'}
+    else:
+        print('building new container for connection: '+ websocket_id)
+        # register new websocket in websocket table
+        send_command("sqlite3 "+path_to_db+" 'insert into websockets VALUES(\""+websocket_id+"\", 1 , 0, 0,0) '")
+        return  send_command(docker_run_command( websocket_id),response=True)
+        
+        
+def respond(err, res=None):
+    return {
+        'statusCode': '400' if err else '200',
+        'body': json.dumps(res),
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
+    
+def lambda_handler(event, context):
+
+    #check if the input is correct
+    try:
+        user_params['asapa_email']  = event['headers']['asapa_email']
+        user_params['asapa_pass']  = event['headers']['asapa_pass']
+
+    except:
+         print("Invalid user credentials")
+         return respond(ValueError('Invaid user credentials'))
+    
+    try:
+        payload = json.loads(event['body'])
+        connection_params['internalID']  = payload['internalID']
+        connection_params['interWorkingInterface']  = payload['interWorkingInterface']
+        connection_params['platformID']  = payload['platformID']
+    except:
+         print("Invalid input.")
+         return respond(ValueError('Unsupported input format '))
+    
+    
+    websocket_id = get_hash(connection_params)
+    state = get_state(websocket_id)
+
+    operations = {
+        'DELETE':   lambda remove_socket, state, websocket_id:  remove_socket(websocket_id),
+        'POST':     lambda open_socket, state, websocket_id:    open_socket(state, websocket_id)
+    }
+
+    operation = event['httpMethod']
+    if operation in operations:
+        function = open_socket if operation == 'POST' else remove_socket
+        response = operations[operation](function, state,  websocket_id)
+        print('the response is:')
+        print(response)
+        err = True if response['Status']== "Failed" else False
+        return respond(err, response)
+    else:
+        return respond(ValueError('Unsupported method "{}"'.format(operation)))
+  
+        
+
+        
